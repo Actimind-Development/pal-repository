57d34535505f9398882a43204333f8487c83f8c0
Actimind-Development <->
2014-10-30 18:32:17 +0400

    Add new function :cylinderVolume

--- src/javascript5/maths3d.js
+++ src/javascript5/maths3d.js
@@ -1,8 +1,15 @@
 
 (function(exports, imports) {
     //::imports
+  var maths = imports.Maths;
     //::module maths3d
     var maths3d = exports.Maths3d = {
+    //::function cylinderVolume
+    //@@depends circleArea
+    cylinderVolume: function(radius, height) {
+        return maths.circleArea(radius) * height;
+    },
+
     //::addbefore
         stub: null
     }
