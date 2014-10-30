aba04f77cb24b65da66eaec3a8cfc50ea9058f5a
Actimind-Development <->
2014-10-30 18:32:16 +0400

    Add new function :cylinderVolume

---
+++ src/javascript5/maths3d.js
@@ -0,0 +1,10 @@
+
+(function(exports, imports) {
+    //::imports
+    //::module maths3d
+    var maths3d = exports.Maths3d = {
+    //::addbefore
+        stub: null
+    }
+})(window, window);
+                    
